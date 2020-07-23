b89f57d08c37bae9265a0e8e5ee4c6ff4b66c17a
edensongjbs <edensongjbs@gmail.com>
2020-07-22 20:37:21 -0400

    working draft no backend

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-// const Tone = require("./Tone");
 
-let readyToAnswer = false
-let currentAnswer = []
 
 // Playing the Keyboard
 
@@ -21,7 +18,17 @@ const keyBindings = {
     "u": "A#3"
 }
 
+const chordBindings = {
+    "I": ["C3", "E3", "G3"],
+    "V": ["G3", "B3", "D3"],
+    "IV": ["F3", "A3", "C4"],
+    "ii": ["D3", "F3", "A3"],
+    "vi": ["A3", "C4", "E3"],
+    "iii": ["E3", "G3", "B3"]
+}
+
 const allNotes = document.querySelectorAll('.white-note, .black-note')
+const allChords = document.querySelectorAll('.chords button')
 
 var synth = new Tone.PolySynth(4, Tone.Synth, {
     oscillator : {
@@ -52,7 +59,24 @@ for (const note of allNotes) {
     note.addEventListener('touchend', () => {
         releaseNote(event.target.id)
     })
-} 
+}
+
+for (const chord of allChords) {
+    chord.addEventListener("mousedown", (e) => {
+        // console.log("heyo")
+        playChord(chordBindings[event.target.innerText])
+    })
+}
+
+playChord = (chord) => {
+    for (const n of chord)
+    {synth.triggerAttackRelease(n, '2n')}
+    if (readyToAnswer && currentQuestion.chordsAllowed) {
+        currentAnswer = chord
+        console.log(currentAnswer)
+        answerRound()
+    }
+}
 
 playNote = (note) => {
     synth.triggerAttack(note)
@@ -68,56 +92,13 @@ releaseNote = (note) => {
     synth.triggerRelease(note);
     const noteDiv = document.querySelector(`#${note.replace("#", '\\#')}`)
     noteDiv.classList.remove('playing')
+    if (readyToAnswer && (currentAnswer.length >= currentQuestion.notes.length)) {answerRound()}
 }
-
-// compPlayNote = (time) => {
-//     console.log("playing")
-//     synth.triggerAttackRelease("C4", "4n", time)
-// }
-
-// scheduleNote
-
-//Together
-// function triggerSynth() {
-//     synth.triggerAttackRelease("C3", "2n", "4n")
-//     synth.triggerAttackRelease("G3", "2n", "8n")
-//     synth.triggerAttackRelease("E3", "2n", "2n")
-// }
-// Tone.Transport.schedule(compPlayNote, 0)
-// Tone.Transport.schedule(compPlayNote, Tone.Time("4n"))
-// Tone.Transport.schedule(compPlayNote, 2*Tone.Time("4n"))
-// Tone.Transport.loopEnd = '2m'
-// Tone.Transport.loop = true
-
-// let compNoteSeq = [[0, "C2"], ["0:2", "C3"], ["0:3:2", "G2"]]
-
-// [[0, "C2"], ["0:2", "C3"], ["0:3:2", "G2"]]);
-
-// const part = new Tone.Part(((time, value) => {
-// 	// the value is an object which contains both the note and the velocity
-//     console.log("playing")
-//     synth.triggerAttackRelease(value.note, value.duration, time, value.velocity);
-// }), [{ time: 0, note: "C3", velocity: 0.9, duration: "8n" },
-// 	{ time: "0:2", note: "C4", velocity: 0.5, duration: "2n" }
-// ])
-
-// part.start(0)
-
-// synth.triggerAttackRelease("C3", "2n").then(() => synth.triggerAttackRelease("G3", "2n")
-// )
 
 
 //Display Logic and Responsiveness
 
 const wrapper = document.querySelector('div.wrapper')
-
-// const part = new Tone.Part(((time, value) => {
-//     // the value is an object which contains both the note and the velocity
-//     console.log("playing")
-//     synth.triggerAttackRelease(value.note, value.duration, time, value.velocity);
-// }), [{ time: 0, note: "C3", velocity: 0.9, duration: "8n" },
-//     { time: "0:2", note: "C4", velocity: 0.5, duration: "2n" }
-// ])
 
 document.addEventListener("DOMContentLoaded", resizeKeybed)
 
@@ -147,43 +128,81 @@ menuButton.addEventListener("click", e => {
 
 // Game Logic
 
+let readyToAnswer = false
+let currentAnswer = []
+
 const Correct = ["That's Right!", "Great Work!", "Fantastic!"]
 const Incorrect = ["Keep Trying!", "So close...", "You'll get it next time!"]
 
-// class Question() {
-//     constructor(notes, question, playItFirst = false, ordered = false) {
-//         this.question=question
-//         this.notes=notes
-//         this.ordered=ordered
-//         this.guesses=0
-//         this.completed=false
-//     }
-//     ask() {
-//         console.log(this.question)
-//     }
-//     compareReponse(res) {
-//         if (!ordered){
-//             res.sort()
-//             this.notes.sort()
-//         }
-//         return (this.notes.find((e, i) => e!==res[i]))
-//     }
-//     badFeedback() {
-//         console.log(Incorrect[(Math.random*Incorrect.length).floor])
-//     }
-//     goodFeedback() {
-//         console.log(Correct[(Math.random*Correct.length).floor])
-//     }
-// }
+class Question {
+    constructor(notes, question, playItFirst = false, ordered = false, chordsAllowed = false) {
+        this.question=question
+        this.notes=notes
+        this.playItFirst=playItFirst
+        this.ordered=ordered
+        this.chordsAllowed=chordsAllowed
+        this.guesses=0
+        this.completed=false
+    }
+    ask() {
+        alert(this.question)
+        if (this.playItFirst) {
+            // alert("woof")
+            playCurrentSequence()
+        }
+    }
+    compareReponse(res) {
+        if (!this.ordered){
+            res.sort()
+            this.notes.sort()
+        }
+        return !(this.notes.find((e, i) => e!==res[i]))
+    }
+    badFeedback() {
+        alert(Incorrect[Math.floor((Math.random()*(Incorrect.length)))])
+    }
+    goodFeedback() {
+        // debugger
+        alert(Correct[Math.floor((Math.random()*(Correct.length)))])
+    }
+}
 
-// const q1 = new Question (["D3", "G3", "B3"], "Play a V chord")
-// const q2 = new Question
-// const q3 = 
+const q1 = new Question (["D3", "G3", "B3"], "Play a V chord", false, false, true)
+const q2 = new Question (["C3", "D3", "F3"], "Listen to the following notes and play them back in order", true, true)
+const q3 = new Question (["F3", "A3", "C4"], "Play back the following chord", true, false, true)
 
-// playGame() {
+let round = [q1, q2, q3]
+
+let currentQuestion //current question index
+
+function dealRound(q) {
+    readyToAnswer = false
+    currentAnswer = []
+    currentQuestion = round[q]
+    if (round[q].playItFirst){loadNewSequence(round[q].notes)}
+    else {loadNewSequence([])}
+    round[q].ask()
+    readyToAnswer = true
+}
+
+function gameOver() {
+    alert('Good Game!')
+}
+
+function answerRound() {
+    readyToAnswer = false
+    if (currentQuestion.compareReponse(currentAnswer)){currentQuestion.goodFeedback()}
+    else {currentQuestion.badFeedback()}
+    let q = round.indexOf(currentQuestion)
+    if (q+1 === round.length) {gameOver()}
+    dealRound(q+1)
+}
+
+function playGame() {
+    dealRound(0)
     // Takes a Game object? array of questions
     // We'll need Question class and possibly Round/Game class(es)
-    
+    // theGameArray.forEach(playRound)
     // For each question:
     // Ask question
     // Reset Answer Array
@@ -193,14 +212,36 @@ const Incorrect = ["Keep Trying!", "So close...", "You'll get it next time!"]
     // Keep track of score
     // Give Response
     // Reset number of tries
-// }
+}
 
 //Question Seeding
 
-let currentCompNotes = ["C4", "E4", "G4"]
+//Playing Questions
+
+let currentCompNotes
 let currentCompTimes = [0, '0:3', '0:6']
 let currentIndex = 0;
 
+function playCurrentSequence() {
+    if (Tone.Transport.state==="started") {Tone.Transport.toggle()}
+    Tone.Transport.toggle()
+}
+
+function clearCurrentSequence() {
+    for (const i in Tone.Transport._scheduledEvents){
+        console.log(i)
+        Tone.Transport.clear(i)
+    }
+}
+
+function loadNewSequence(ar) {
+    clearCurrentSequence()
+    currentCompNotes = ar
+    currentIndex = 0
+    setup()    
+}
+
+// loadNewSequence(["C4", "E4", "G4"])
 
 function inc() {
     if (currentIndex===(currentCompNotes.length-1)){
@@ -209,35 +250,6 @@ function inc() {
     else {currentIndex++}
 }
 
-// class NoteSequence {
-//     constructor(pitches, phraseLength = '1m'){
-//         this.pitches=pitches
-//         this.phraseLength=phraseLength
-//         this.currentIndex=0;
-//     }
-//     get current(){
-//         return this.pitches[this.currentIndex]
-//     }
-//     inc() {
-//         if (this.currentIndex===(this.pitches.length-1))
-//         {
-//             this.currentIndex=0
-//         }
-//         else {this.currentIndex++}
-//     }
-//     setup() {
-
-//         for (const p in this.pitches) {
-//             Tone.Transport.schedule(((this.triggerSynth).bind(this)), p.time)
-//         }
-//         // Tone.Transport.schedule(this.triggerSynth, '0:2')
-//         // Tone.Transport.schedule(this.triggerSynth, '0:2:2.5')
-
-//         //set the transport to repeat
-//         Tone.Transport.loopEnd = this.phraseLength
-//         Tone.Transport.loop = true
-//     }
-// }
 
 function setup() {
 
@@ -259,19 +271,5 @@ function triggerSynth(time){
     inc()
 }
 
-// let ns = new NoteSequence([{note: "C4", duration: "4n" ,time: 0}, {note: "E4", duration: "4n" ,time: '0:2'}, {note: "G4", duration: "2n" ,time: '0:3'}]) 
-// ns.setup()
-
-// function triggerSynth(time){
-// 	//the time is the sample-accurate time of the event
-// 	synth.triggerAttackRelease('C2', '8n', time)
-// }
-
-// //schedule a few notes
-// Tone.Transport.schedule(triggerSynth, 0)
-// Tone.Transport.schedule(triggerSynth, '0:2')
-// Tone.Transport.schedule(triggerSynth, '0:2:2.5')
-
-// //set the transport to repeat
-// Tone.Transport.loopEnd = '1m'
-// Tone.Transport.loop = false
+playButton = document.querySelector('.play')
+playButton.addEventListener("click", playGame)
